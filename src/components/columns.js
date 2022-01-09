40bd9dc2a728a2f1d04977fd83d11370cb78a886
Piyush <piyushyadav11102002@gmail.com>
2022-01-09 14:34:31 +0530

    feat: homeppage

---
+++ src/components/columns.js
@@ -0,0 +1,26 @@
+export const COLUMNS = [
+    {
+        Header: 'Id',
+        accessor: 'id'
+    },
+    {
+        Header: 'Name',
+        accessor: 'name'
+    },
+    {
+        Header: 'Gender',
+        accessor: 'gender'
+    },
+    {
+        Header: 'Position',
+        accessor: 'position'
+    },
+    {
+        Header: 'Stage',
+        accessor: 'stage'
+    },
+    {
+        Header: 'CV',
+        accessor: 'cv'
+    }
+]
